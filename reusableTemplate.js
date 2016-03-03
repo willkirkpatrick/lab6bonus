7c706d0703b75a63dc83272acd7d012eb8a1915e
Will Kirkpatrick <willkirkpatrick@gmail.com>
2016-03-03 14:31:09 -0600

    completed through pluck

--- reusableTemplate.js
+++ reusableTemplate.js
@@ -1,25 +1,86 @@
 var library = (function(){
 	return {
 		// Utility --- Complete Functions Below
-		identity : function(val) {},
+		identity : function(val) {
+            return val;
+        },
 
 		// Collections --- Complete Functions Below
-		each : function(list, iterator) {},
+		each : function(list, iterator) { 
+             if (Array.isArray(list)) { 
+                 for (var i = 0; i < list.length; i++) {     
+                iterator(list[i], i, list);
+                }
+                }else { 
+               var x;
+                for (x in list) {
+               iterator(list[x], x, list);
+                }
+            }
+        }, 
 
-		filter : function(list, test) {},
+		filter : function(list, test) {
+            var newArray = []
+             for (var i = 0; i < list.length; i++) {
+               if (test(list[i])) {
+                    newArray.push(list[i]);
+               }
+            }
+            
+            return newArray;
+        },
 
-		reject : function(list, test) {},
+		reject : function(list, test) {
+             var newArray = []
+              for (var i = 0; i < list.length; i++) {
+                if (!test(list[i])) {
+                    newArray.push(list[i]);
+               }
+            }
+             return newArray;
+        },
 
-		map : function(list, iterator) {},
+		map : function(list, iterator) {
+            var newArray = [];
+            for (var i = 0; i < list.length; i++) {
+                 var x = iterator(list[i]);
+                newArray.push(x);
+            }
+            return newArray;
+        },
 
 		pluck : function(list, key) {
 			return this.map(list, function(item){
 				return item[key];
 			});
 		},
-		reduce : function(list, iterator, accumulator) {},
-
-		every : function(list, iterator) {},
+		reduce : function(list, iterator, accumulator) {
+            // console.log(accumulator);
+            //      for (var i = 0; i < list.length; i++) {
+            //         if (accumulator == 0) {
+            //             accumulator = list[i];
+            //             console.log(accumulator);
+            //         }
+            //         accumulator = iterator(list[i], accumulator);
+            //         console.log(accumulator);
+            //         return accumulator;
+            //     }
+                
+            console.log(list);
+            console.log(iterator);
+            console.log(accumulator);
+            if (accumulator === undefined) {
+                accumulator = list[0];
+            }
+            
+            accumulator = accumulator + list.forEach(iterator(list));
+            console.log(accumulator);
+            return accumulator; 
+        },
+		every : function(list, iterator) {
+            // console.log(list);
+            // console.log(iterator);
+        },
 
 		some : function(list, iterator) {},
 
